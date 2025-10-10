94b818344f8afc745b308a2d393f8324754dc088
abby001y <abbypriyoguna@gmail.com>
2025-10-10 22:28:09 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/HelloProcedural.java
@@ -0,0 +1,9 @@
+// HelloProcedural
+public class HelloProcedural {
+    public static void main(String[] args) {
+        String nim = "240202848";
+        String name = "abbi priyoguno";
+
+        System.out.println("Hello World, I am " + name + " - " + nim);
+    }
+}
